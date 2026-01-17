82827c403c32780b7c2068c77f09bc786a5d6abc
scalerflux <mdomeyr@gmail.com>
2026-01-17 19:29:05 +0530

    Update cycle_calc.v

--- Day3/code/cycle_calc.v
+++ Day3/code/cycle_calc.v
@@ -1,4 +1,19 @@
- |calc 
+\m5_TLV_version 1d: tl-x.org
+\m5
+// Done by Mohammed Omer(RISCV-MYTH workshop)
+   // ============================================
+   // Welcome, new visitors! Try the "Learn" menu.
+   // ============================================
+   
+   //use(m5-1.0)   /// uncomment to use M5 macro library.
+\SV
+   // Macro providing required top-level module definition, random
+   // stimulus support, and Verilator config.
+   m5_makerchip_module   // (Expanded in Nav-TLV pane.)
+   m4_include_lib(['https://raw.githubusercontent.com/stevehoover/LF-Building-a-RISC-V-CPU-Core/main/lib/calc_viz.tlv'])
+\TLV
+   $reset = *reset;
+  |calc 
       @1             
          $val1[31:0] = >>2$out;   
          $cnt = reset ? 0 : (>>1$cnt + 1);  
