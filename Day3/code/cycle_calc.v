e91acbb8a782e3ae7cd9161c529ec643c0593c77
scalerflux <mdomeyr@gmail.com>
2026-01-19 10:28:14 +0530

    Update cycle_calc.v

--- Day3/code/cycle_calc.v
+++ Day3/code/cycle_calc.v
@@ -1,6 +1,6 @@
 \m5_TLV_version 1d: tl-x.org
 \m5
-// Done by Mohammed Omer(RISCV-MYTH workshop)
+   // Mohammed Omer (RISCV-MYTH workshop)
    // ============================================
    // Welcome, new visitors! Try the "Learn" menu.
    // ============================================
@@ -10,13 +10,13 @@
    // Macro providing required top-level module definition, random
    // stimulus support, and Verilator config.
    m5_makerchip_module   // (Expanded in Nav-TLV pane.)
-   m4_include_lib(['https://raw.githubusercontent.com/stevehoover/LF-Building-a-RISC-V-CPU-Core/main/lib/calc_viz.tlv'])
 \TLV
    $reset = *reset;
-  |calc 
+   
+   |calc 
       @1             
          $val1[31:0] = >>2$out;   
-         $cnt = reset ? 0 : (>>1$cnt + 1);  
+         $cnt[0] = $reset ? 0 : (>>1$cnt + 1);  
          $valid = ! $cnt;
    
          $sum[31:0] = $val1[31:0] + $val2[31:0];
@@ -25,7 +25,13 @@
          $quot[31:0] = $val1 / $val2;
       @2
          $out[31:0] = ($reset || $valid) ? 32'b0:
-                      $op == 2'b00 ? $sum :
+                      $op[1:0] == 2'b00 ? $sum :
                       $op == 2'b01 ? $diff :
                       $op == 2'b10 ? $prod :
                                       $quot;
+   
+   // Assert these to end simulation (before the cycle limit).
+   *passed = *cyc_cnt > 40;
+   *failed = 1'b0;
+\SV
+   endmodule
